b65dbc138ab26ef1e1f5b407018382293b15cd1b
pelaezluis <luispelaezbedoya@gmail.com>
2026-05-26 13:00:49 -0500

    debug

--- config.py
+++ config.py
@@ -6,12 +6,21 @@ class Connection:
 
     @classmethod
     def get_connection(cls):
+        host = os.environ.get("DB_HOST", "localhost")
+        user = os.environ.get("DB_USER", "root")
+        password = os.environ.get("DB_PASSWORD", "")
+        database = os.environ.get("DB_NAME", "blog")
+        port = int(os.environ.get("DB_PORT", 3306))
+
+        print(f"[DB CONFIG] host={host} user={user} db={database} port={port}")
+        print(f"[DB CONFIG] password_len={len(password)}")
+
         return connector.connect(
-            host=os.environ.get("DB_HOST", "localhost"),
-            user=os.environ.get("DB_USER", "root"),
-            password=os.environ.get("DB_PASSWORD", ""),
-            database=os.environ.get("DB_NAME", "blog"),
-            port=int(os.environ.get("DB_PORT", 3306)),
+            host=host,
+            user=user,
+            password=password,
+            database=database,
+            port=port,
             charset="utf8mb4",
             use_unicode=True,
         )
